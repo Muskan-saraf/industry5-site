9b86d9c57640efb1cdc2fb69a71c8300cf4ad41e
Muskan-saraf <Muskan-saraf@users.noreply.github.com>
2025-03-09 18:23:20 +0000

    deploy: 636b58288d37f74ff674b955d72b3107740b0945

---
+++ assets/about.md.b6d02a1f.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a,a as i,b as r}from"./app.b64dae54.js";const v=JSON.parse('{"title":"About Industry 5.0 Insights","description":"","frontmatter":{},"headers":[],"relativePath":"about.md"}'),n={name:"about.md"},s=i("",24),o=[s];function d(h,l,u,c,m,g){return t(),a("div",null,o)}const b=e(n,[["render",d]]);export{v as __pageData,b as default};
